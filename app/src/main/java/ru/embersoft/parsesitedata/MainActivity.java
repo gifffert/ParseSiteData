8f81a0540d3414ca1de28be5ce4aa0ddac4d51bc
Nick Titov <titonick174@icloud.com>
2019-10-19 21:34:34 +0900

    Initial commit

---
+++ app/src/main/java/ru/embersoft/parsesitedata/MainActivity.java
@@ -0,0 +1,95 @@
+package ru.embersoft.parsesitedata;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.view.animation.AnimationUtils;
+import android.widget.ProgressBar;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.select.Elements;
+
+import java.io.IOException;
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+
+    private RecyclerView recyclerView;
+    private ParseAdapter adapter;
+    private ArrayList<ParseItem> parseItems = new ArrayList<>();
+    private ProgressBar progressBar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        progressBar = findViewById(R.id.progressBar);
+        recyclerView = findViewById(R.id.recyclerView);
+
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        adapter = new ParseAdapter(parseItems, this);
+        recyclerView.setAdapter(adapter);
+
+        Content content = new Content();
+        content.execute();
+    }
+
+    private class Content extends AsyncTask<Void,Void,Void>{
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            progressBar.setVisibility(View.VISIBLE);
+            progressBar.startAnimation(AnimationUtils.loadAnimation(MainActivity.this, android.R.anim.fade_in));
+        }
+
+        @Override
+        protected void onPostExecute(Void aVoid) {
+            super.onPostExecute(aVoid);
+            progressBar.setVisibility(View.GONE);
+            progressBar.startAnimation(AnimationUtils.loadAnimation(MainActivity.this, android.R.anim.fade_out));
+            adapter.notifyDataSetChanged();
+        }
+
+        @Override
+        protected Void doInBackground(Void... voids) {
+
+            try {
+                String url = "https://www.cinemaqatar.com/";
+
+                Document doc = Jsoup.connect(url).get();
+
+                Elements data = doc.select("span.thumbnail");
+                int size = data.size();
+                for (int i = 0; i < size; i++) {
+                    String imgUrl = data.select("span.thumbnail")
+                            .select("img")
+                            .eq(i)
+                            .attr("src");
+
+                    String title = data.select("h4.gridminfotitle")
+                            .select("span")
+                            .eq(i)
+                            .text();
+
+                    parseItems.add(new ParseItem(imgUrl, title));
+                    Log.d("items", "img: " + imgUrl + " . title: " + title);
+                }
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+
+            return null;
+        }
+    }
+}
---
+++ app/src/main/java/ru/embersoft/parsesitedata/ParseAdapter.java
@@ -0,0 +1,57 @@
+package ru.embersoft.parsesitedata;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.squareup.picasso.Picasso;
+
+import java.util.ArrayList;
+
+public class ParseAdapter extends RecyclerView.Adapter<ParseAdapter.ViewHolder> {
+
+    private ArrayList<ParseItem> parseItems;
+    private Context context;
+
+    public ParseAdapter(ArrayList<ParseItem> parseItems, Context context) {
+        this.parseItems = parseItems;
+        this.context = context;
+    }
+
+    @NonNull
+    @Override
+    public ParseAdapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.parse_item, parent, false);
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ParseAdapter.ViewHolder holder, int position) {
+        ParseItem parseItem = parseItems.get(position);
+        holder.textView.setText(parseItem.getTitle());
+        Picasso.get().load(parseItem.getImgUrl()).into(holder.imageView);
+    }
+
+    @Override
+    public int getItemCount() {
+        return parseItems.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder{
+
+        ImageView imageView;
+        TextView textView;
+
+        public ViewHolder(@NonNull View view) {
+            super(view);
+            imageView = view.findViewById(R.id.imageView);
+            textView = view.findViewById(R.id.textView);
+        }
+    }
+}
